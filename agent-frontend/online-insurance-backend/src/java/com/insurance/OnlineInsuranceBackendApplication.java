17b8b1349342371bfafb89671ea3fcc3bb3c04b5
Nandhuarumalla <arumallanandini32@gmail.com>
2026-02-11 14:39:39 +0530

    Add files via upload

---
+++ agent-frontend/online-insurance-backend/src/java/com/insurance/OnlineInsuranceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineInsuranceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineInsuranceBackendApplication.class, args);
+	}
+
+}
